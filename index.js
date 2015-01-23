1b78dd1fae9fe6eeaf369226bf039c3a30ce4784
John Otander <johnotander@gmail.com>
2015-01-23 10:55:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function commaSplit(options) {
+  options = options || {};
+
+  return true;
+}
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var commaSplit = require('..');
+
+describe('comma-split', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(commaSplit(), true);
+  });
+});
