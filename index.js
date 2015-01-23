af8b686a1734e04a55993216ee63e1661eb97a18
John Otander <johnotander@gmail.com>
2015-01-23 11:07:36 -0700

    Add initial functionality

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-module.exports = function commaSplit(options) {
+module.exports = function commaSplit(stringToSplit, options) {
+  if (typeof stringToSplit != 'string') {
+    throw new TypeError('comma-split expects a string');
+  }
+
   options = options || {};
 
-  return true;
+  if (options.ignoreWhitespace) {
+    var splitRegex = /,/;
+  } else if (options.ignoreTrailingWhitespace) {
+    var splitRegex = /,\s*/;
+  } else if (options.ignoreLeadingWhitespace) {
+    var splitRegex = /\s*,/;
+  } else {
+    var splitRegex = /\s*,\s*/;
+  }
+
+  return stringToSplit.split(splitRegex);
 }
--- test/test.js
+++ test/test.js
@@ -3,7 +3,38 @@ var commaSplit = require('..');
 
 describe('comma-split', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(commaSplit(), true);
+  it('should do split on commas, and remove surrounding whitespace', function() {
+    assert.deepEqual(
+      commaSplit('some, string,\r\r\n\tdelimited\n,by, commas'),
+      ['some','string','delimited','by','commas']
+    );
+  });
+
+  it('should split on commas and ignore whitespace', function() {
+    assert.deepEqual(
+      commaSplit('some, string,\r\r\n\tdelimited\n,by, commas', { ignoreWhitespace: true }),
+      ['some',' string','\r\r\n\tdelimited\n','by',' commas']
+    );
+  });
+
+  it('should split on commas and ignore trailing whitespace', function() {
+    assert.deepEqual(
+      commaSplit('some, string,\r\r\n\tdelimited\n,by, commas', { ignoreTrailingWhitespace: true }),
+      ['some','string','delimited\n','by','commas']
+    );
+  });
+
+  it('should split on commas and ignore leading whitespace', function() {
+    assert.deepEqual(
+      commaSplit('some, string,\r\r\n\tdelimited\n,by, commas', { ignoreLeadingWhitespace: true }),
+      ['some',' string','\r\r\n\tdelimited','by',' commas']
+    );
+  });
+
+  it('should be able to handle malformed comma delimited lists', function() {
+    assert.deepEqual(
+      commaSplit(',,some, string,\r\r\n\tdelimited\n,by, commas'),
+      ['','','some','string','delimited','by','commas']
+    );
   });
 });
